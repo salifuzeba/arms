b53091de27e39f1ea71e4c77243c617e5aff93ec
salifuzeba <85734088+salifuzeba@users.noreply.github.com>
2021-06-11 09:15:49 +0000

    Initial commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,23 @@
+const targetUrl = 'http://ec2-54-229-159-97.eu-west-1.compute.amazonaws.com/';
+
+describe('Testing login for ARMS', () => {
+    let data
+    before(() => {
+        cy.clearCookies()
+        cy.fixture('login').then(result => {
+            data = result
+        })
+        cy.visit(`${targetUrl}`)
+    })
+
+    it('login to arms with correct email and password', () => {
+        cy.get('#email').type(data.correctDetails.email)
+        cy.get('#password').type(data.correctDetails.password)
+        cy.get("button").contains("Login").click()
+    })
+
+})
+describe("testing staffing tools", ()=>{
+    it("staffing tools testv", () =>{
+        cy.get('#navbarSupportedContent > :nth-child(1) > :nth-child(4)').click()
+})
